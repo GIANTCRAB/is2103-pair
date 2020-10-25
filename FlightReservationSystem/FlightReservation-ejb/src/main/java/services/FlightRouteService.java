1bdba50c2e06a25d1cf961db01fcb8afadef5bf8
Huiren Woo <giantcrabby@gmail.com>
2020-10-26 03:40:30 +0800

    Add flight route service

---
+++ FlightReservationSystem/FlightReservation-ejb/src/main/java/services/FlightRouteService.java
@@ -0,0 +1,40 @@
+package services;
+
+import entities.Airport;
+import entities.FlightRoute;
+import exceptions.InvalidConstraintException;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+@LocalBean
+@Stateless
+public class FlightRouteService {
+    @PersistenceContext(unitName = "frs")
+    private EntityManager em;
+
+    private final ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+    private final Validator validator = validatorFactory.getValidator();
+
+    public FlightRoute create(Airport origin, Airport destination) throws InvalidConstraintException {
+        final FlightRoute flightRoute = new FlightRoute();
+        flightRoute.setOrigin(origin);
+        flightRoute.setDest(destination);
+        Set<ConstraintViolation<FlightRoute>> violations = this.validator.validate(flightRoute);
+        // There are invalid data
+        if (!violations.isEmpty()) {
+            throw new InvalidConstraintException(violations.toString());
+        }
+        this.em.persist(flightRoute);
+        this.em.flush();
+
+        return flightRoute;
+    }
+}
