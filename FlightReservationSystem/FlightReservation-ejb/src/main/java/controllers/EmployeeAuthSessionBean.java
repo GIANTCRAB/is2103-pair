2c04d79dcaa00187afa1c7af0628fcaded8474b7
Huiren Woo <giantcrabby@gmail.com>
2020-10-26 02:28:36 +0800

    Add employee auth session bean

---
+++ FlightReservationSystem/FlightReservation-ejb/src/main/java/controllers/EmployeeAuthSessionBean.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import entities.Employee;
+import exceptions.IncorrectCredentialsException;
+import services.AuthService;
+
+import javax.ejb.Stateful;
+import javax.inject.Inject;
+
+/**
+ * The auth session bean holds zero knowledge about entity manager. The purpose of this bean to parse certain data and route the data to services
+ */
+@Stateful
+public class EmployeeAuthSessionBean implements EmployeeAuthBeanRemote {
+    @Inject
+    AuthService authService;
+
+    @Override
+    public Employee login(String username, String password) throws IncorrectCredentialsException {
+        return this.authService.login(username, password);
+    }
+}
---
+++ FlightReservationSystem/FlightReservation-ejb/src/main/java/services/AuthService.java
@@ -0,0 +1,45 @@
+package services;
+
+import entities.Employee;
+import exceptions.IncorrectCredentialsException;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.security.enterprise.identitystore.Pbkdf2PasswordHash;
+
+@LocalBean
+@Stateless
+public class AuthService {
+    @PersistenceContext(unitName = "frs")
+    private EntityManager em;
+    @Inject
+    private Pbkdf2PasswordHash passwordHash;
+
+    public Employee login(String username, String password) throws IncorrectCredentialsException {
+        final TypedQuery<Employee> searchQuery = this.em.createQuery("select e from Employee e where e.username = ?1", Employee.class)
+                .setParameter(1, username);
+
+        try {
+            final Employee searchResult = searchQuery.getSingleResult();
+
+            // WARNING: If password isn't hashed, just allow it to compare anyways. Don't do this in real world! Always hash your passwords
+            try {
+                if (this.passwordHash.verify(password.toCharArray(), searchResult.getPassword())) {
+                    return searchResult;
+                }
+            } catch (IllegalArgumentException e) {
+                if (password.equals(searchResult.getPassword())) {
+                    return searchResult;
+                }
+            }
+        } catch (NoResultException ignored) {
+        }
+
+        throw new IncorrectCredentialsException();
+    }
+}
