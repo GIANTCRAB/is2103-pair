e0a7326d99073f6a6da4df37a060cea2731f27f2
Huiren Woo <giantcrabby@gmail.com>
2020-10-26 01:06:45 +0800

    Add airport entity

---
+++ FlightReservationSystem/FlightReservationLibrary/src/main/java/entities/Airport.java
@@ -0,0 +1,46 @@
+package entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Airport implements Serializable {
+    @Id
+    @NotNull
+    @Size(min = 2, max = 3)
+    @Column(length = 3, nullable = false)
+    private String iataCode;
+
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(nullable = false)
+    private String airportName;
+
+    @NotNull
+    @Size(min = 1, max = 127)
+    @Column(length = 127, nullable = false)
+    private String city;
+
+    @NotNull
+    @Size(min = 1, max = 127)
+    @Column(length = 127, nullable = false)
+    private String stateName;
+
+    @NotNull
+    @Size(min = 1, max = 127)
+    @Column(length = 127, nullable = false)
+    private String country;
+}
