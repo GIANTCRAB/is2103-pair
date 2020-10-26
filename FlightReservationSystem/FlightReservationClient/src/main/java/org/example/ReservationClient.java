0dc8ee0f000c5d1baf420d559f44c36e96d36c2f
Huiren Woo <giantcrabby@gmail.com>
2020-10-27 02:25:33 +0800

    Add customer registration UI

--- FlightReservationSystem/FlightReservationClient/src/main/java/org/example/ReservationClient.java
+++ FlightReservationSystem/FlightReservationClient/src/main/java/org/example/ReservationClient.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import controllers.VisitorBeanRemote;
+import entities.Customer;
+import exceptions.InvalidConstraintException;
 import lombok.AccessLevel;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -23,5 +25,64 @@ public class ReservationClient implements SystemClient {
     @Override
     public void runApp() {
         this.scanner = new Scanner(System.in);
+        this.displayVisitorMenu();
+        this.scanner.close();
+    }
+
+    private void displayVisitorMenu() {
+        boolean loop = true;
+
+        while (loop) {
+            System.out.println("*** Flight Reservation Client ***");
+            System.out.println("1: Registration");
+            System.out.println("2: Customer Login");
+            System.out.println("3: Search Flight");
+            System.out.println("4: Exit");
+            final int option = this.scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    displayRegistrationMenu();
+                    break;
+                case 4:
+                default:
+                    loop = false;
+                    break;
+            }
+        }
+    }
+
+    private void displayRegistrationMenu() {
+        boolean loop = true;
+
+        while (loop) {
+            System.out.println("Enter First Name:");
+            final String firstName = this.scanner.next();
+            System.out.println("Enter Last Name:");
+            final String lastName = this.scanner.next();
+            System.out.println("Enter Email:");
+            final String email = this.scanner.next();
+            System.out.println("Enter Password:");
+            final String password = this.scanner.next();
+            System.out.println("Enter Phone Number:");
+            final String phoneNumber = this.scanner.next();
+            System.out.println("Enter Address:");
+            final String address = this.scanner.next();
+
+
+            try {
+                final Customer customer = this.visitorBeanRemote.register(firstName, lastName, email, password, phoneNumber, address);
+                System.out.println("Successfully registered as " + customer.getFirstName() + " (ID: " + customer.getCustomerId() + ")");
+                loop = false;
+            } catch (InvalidConstraintException e) {
+                this.displayConstraintErrorMessage(e);
+            }
+        }
+
+    }
+
+    private void displayConstraintErrorMessage(InvalidConstraintException invalidConstraintException) {
+        System.out.println("There were some validation errors!");
+        System.out.println(invalidConstraintException.toString());
     }
 }
