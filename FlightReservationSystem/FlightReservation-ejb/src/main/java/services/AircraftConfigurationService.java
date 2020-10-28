4df97318a98fe4a4e46a02a3544306eb3bfacec9
Huiren Woo <giantcrabby@gmail.com>
2020-10-28 18:33:41 +0800

    Change to enforce cabin class size at service level

--- FlightReservationSystem/FlightReservation-ejb/src/main/java/services/AircraftConfigurationService.java
+++ FlightReservationSystem/FlightReservation-ejb/src/main/java/services/AircraftConfigurationService.java
@@ -3,45 +3,51 @@ package services;
 import entities.AircraftConfiguration;
 import entities.CabinClass;
 import entities.AircraftType;
+import exceptions.InvalidEntityIdException;
+
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
 @LocalBean
 @Stateless
 public class AircraftConfigurationService {
+    private final int MIN_CABIN_CLASS_SIZE = 1;
+    private final int MAX_CABIN_CLASS_SIZE = 4;
 
     @PersistenceContext(unitName = "frs")
     private EntityManager em;
-    
-    public AircraftConfiguration create(String aircraftConfigurationName, int noOfCabinClasses, List<CabinClass> cabinClassList, AircraftType aircraftType) {
+
+    public AircraftConfiguration create(String aircraftConfigurationName, List<CabinClass> cabinClassList, AircraftType aircraftType) throws InvalidEntityIdException {
+        if (cabinClassList.size() < MIN_CABIN_CLASS_SIZE || cabinClassList.size() > MAX_CABIN_CLASS_SIZE) {
+            throw new InvalidEntityIdException();
+        }
+
         final AircraftConfiguration newAircraftConfiguration = new AircraftConfiguration();
         newAircraftConfiguration.setAircraftConfigurationName(aircraftConfigurationName);
-        newAircraftConfiguration.setNoOfCabinClasses(noOfCabinClasses);
         newAircraftConfiguration.setCabinClasses(cabinClassList);
         newAircraftConfiguration.setAircraftType(aircraftType);
-        
+
         this.em.persist(newAircraftConfiguration);
         this.em.flush();
-        
+
         return newAircraftConfiguration;
     }
-    
+
     public List<AircraftConfiguration> getAircraftConfigurations() {
         final TypedQuery<AircraftConfiguration> searchQuery = this.em.createQuery("SELECT ac from AircraftConfiguration ac", AircraftConfiguration.class);
 
         return searchQuery.getResultList();
     }
-    
+
     public AircraftConfiguration findAircraftConfigurationByName(String name) {
-        final Query searchQuery = this.em.createQuery(
-                "SELECT ac from AircraftConfiguration ac WHERE ac.aircraftConfigurationName LIKE ?1")
+        final TypedQuery<AircraftConfiguration> searchQuery = this.em.createQuery(
+                "SELECT ac from AircraftConfiguration ac WHERE ac.aircraftConfigurationName LIKE ?1", AircraftConfiguration.class)
                 .setParameter(1, name);
-        
-        return (AircraftConfiguration)searchQuery.getSingleResult();
+
+        return searchQuery.getSingleResult();
     }
 }
