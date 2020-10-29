2a250bf9648532d3b1582844c1dccff285037cc6
Huiren Woo <giantcrabby@gmail.com>
2020-10-29 18:14:42 +0800

    Fix FlightRoute check

--- FlightReservationSystem/FlightReservationLibrary/src/main/java/entities/FlightRoute.java
+++ FlightReservationSystem/FlightReservationLibrary/src/main/java/entities/FlightRoute.java
@@ -7,8 +7,9 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -21,15 +22,16 @@ public class FlightRoute implements Serializable {
 
     @NotNull
     @MapsId("originId")
-    @Size(min = 2, max = 3)
     @ManyToOne
     @JoinColumn(name = "originId", nullable = false)
     private Airport origin;
 
     @NotNull
     @MapsId("destId")
-    @Size(min = 2, max = 3)
     @ManyToOne
     @JoinColumn(name = "destId", nullable = false)
     private Airport dest;
+
+    @OneToMany(mappedBy = "flightRoute")
+    private List<Flight> flights = new ArrayList<>();
 }
