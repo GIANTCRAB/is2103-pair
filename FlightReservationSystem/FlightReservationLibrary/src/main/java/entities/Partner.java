91bd3b37b6bb6e178ba3acbf4f93036174cc8d1f
Huiren Woo <giantcrabby@gmail.com>
2020-11-02 04:52:56 +0800

    Add partner entity

---
+++ FlightReservationSystem/FlightReservationLibrary/src/main/java/entities/Partner.java
@@ -0,0 +1,38 @@
+package entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Partner {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long partnerId;
+
+    @Size(min = 1, max = 127)
+    @Column(length = 127, nullable = false)
+    @NotNull
+    private String companyName;
+
+    @Size(min = 5, max = 32)
+    @Column(length = 32, nullable = false, unique = true)
+    @NotNull
+    private String username;
+
+    @NotNull
+    @Size(min = 3, max = 255)
+    @Column(nullable = false)
+    private String password;
+}
