c2c85ee3900df174a5cdc8394de6985ce79aa837
Blueberry <55194781+blueberry-byte@users.noreply.github.com>
2020-11-05 01:11:00 +0800

    Fixed missing import for Fare

--- FlightReservationSystem/FlightReservationLibrary/src/main/java/entities/Fare.java
+++ FlightReservationSystem/FlightReservationLibrary/src/main/java/entities/Fare.java
@@ -10,6 +10,7 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -20,6 +21,10 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Fare implements Serializable {
 
     private static final long serialVersionUID = 1L;
