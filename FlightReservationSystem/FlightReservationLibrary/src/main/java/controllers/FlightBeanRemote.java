0da71887e18f090ad3f893e738eb1c638baf76f2
Huiren Woo <giantcrabby@gmail.com>
2020-11-07 11:00:32 +0800

    Fix FlightBeanRemote

--- FlightReservationSystem/FlightReservationLibrary/src/main/java/controllers/FlightBeanRemote.java
+++ FlightReservationSystem/FlightReservationLibrary/src/main/java/controllers/FlightBeanRemote.java
@@ -5,17 +5,20 @@ import entities.Flight;
 import exceptions.InvalidConstraintException;
 import exceptions.InvalidEntityIdException;
 import exceptions.NotAuthenticatedException;
+
+import javax.ejb.Remote;
 import java.util.List;
 
+@Remote
 public interface FlightBeanRemote {
-    
-    public Flight create(Employee employee, String flightCode, String origin, String destination, Long aircraftConfigurationId) throws InvalidConstraintException, InvalidEntityIdException, NotAuthenticatedException;
-    
-    public boolean addReturnFlight(Employee employee, String mainFlightCode, String returnFlightCode) throws NotAuthenticatedException;
-    
-    public boolean checkFlightRoute(Employee employee, String origin, String destination) throws InvalidEntityIdException, NotAuthenticatedException;
-    
-    public List<Flight> getFlights(Employee employee) throws NotAuthenticatedException;
-    
-    public Flight getFlightByFlightCode(Employee employee, String flightCode) throws NotAuthenticatedException;
+
+    Flight create(Employee employee, String flightCode, String origin, String destination, Long aircraftConfigurationId) throws InvalidConstraintException, InvalidEntityIdException, NotAuthenticatedException;
+
+    boolean addReturnFlight(Employee employee, String mainFlightCode, String returnFlightCode) throws NotAuthenticatedException;
+
+    boolean checkFlightRoute(Employee employee, String origin, String destination) throws InvalidEntityIdException, NotAuthenticatedException;
+
+    List<Flight> getFlights(Employee employee) throws NotAuthenticatedException;
+
+    Flight getFlightByFlightCode(Employee employee, String flightCode) throws NotAuthenticatedException;
 }
