f699bc7be1b7fa58889dd88723d1790a1c85fabc
Huiren Woo <giantcrabby@gmail.com>
2020-11-07 15:54:19 +0800

    Add more data loading to DataMigrationBean

--- FlightReservationSystem/FlightReservation-ejb/src/main/java/initialisation/DataMigrationBean.java
+++ FlightReservationSystem/FlightReservation-ejb/src/main/java/initialisation/DataMigrationBean.java
@@ -1,9 +1,10 @@
 package initialisation;
 
-import entities.Airport;
-import entities.AircraftType;
-import entities.Employee;
-import entities.EmployeeRole;
+import entities.*;
+import lombok.SneakyThrows;
+import services.AircraftConfigurationService;
+import services.CabinClassService;
+import services.FlightService;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
@@ -19,17 +20,31 @@ public class DataMigrationBean {
     @PersistenceContext(unitName = "frs")
     private EntityManager em;
     @Inject
+    AircraftConfigurationService aircraftConfigurationService;
+    @Inject
+    CabinClassService cabinClassService;
+    @Inject
+    FlightService flightService;
+    @Inject
     private Pbkdf2PasswordHash passwordHash;
+
+    private final Airport sinAirport = new Airport();
+    private final Airport nrtAirport = new Airport();
+    private final FlightRoute sinToNrtFR = new FlightRoute();
+    private AircraftType sinNrtAT;
+    private AircraftConfiguration sinNrtAC = new AircraftConfiguration();
 
     @PostConstruct
     public void init() {
         this.initAirportData();
         this.initAircraftTypeData();
+        this.initFlightRouteData();
+        this.initAircraftConfiguration();
+        this.initFlightData();
         this.initEmployeeData();
     }
 
     private void initAirportData() {
-        final Airport sinAirport = new Airport();
         sinAirport.setAirportName("Singapore Airport");
         sinAirport.setIataCode("SIN");
         sinAirport.setCity("Singapore");
@@ -45,6 +60,13 @@ public class DataMigrationBean {
         sfoAirport.setStateName("California");
         em.persist(sfoAirport);
 
+        nrtAirport.setAirportName("Narita International Airport");
+        nrtAirport.setIataCode("NRT");
+        nrtAirport.setCity("Narita");
+        nrtAirport.setCountry("Japan");
+        nrtAirport.setStateName("Chiba");
+        em.persist(nrtAirport);
+
         em.flush();
     }
 
@@ -59,7 +81,27 @@ public class DataMigrationBean {
         boeingSecondType.setMaxCapacity(660);
         em.persist(boeingSecondType);
 
+        sinNrtAT = boeingSecondType;
+
         em.flush();
+    }
+
+    private void initFlightRouteData() {
+        sinToNrtFR.setOrigin(sinAirport);
+        sinToNrtFR.setDest(nrtAirport);
+        em.persist(sinToNrtFR);
+    }
+
+    @SneakyThrows
+    private void initAircraftConfiguration() {
+        sinNrtAC = this.aircraftConfigurationService.create("basic", sinNrtAT);
+
+        cabinClassService.create(CabinClassType.F, 3,  "3-2-3", sinNrtAC);
+    }
+
+    @SneakyThrows
+    private void initFlightData() {
+        this.flightService.create("ML123", sinToNrtFR, sinNrtAC);
     }
 
     private void initEmployeeData() {
