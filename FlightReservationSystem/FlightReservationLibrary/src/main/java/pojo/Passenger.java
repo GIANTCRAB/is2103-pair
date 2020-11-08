56bef577f7cd84e6fba3e800dcb52ff2cba4f274
Huiren Woo <giantcrabby@gmail.com>
2020-11-09 05:14:38 +0800

    Add serializable for Passenger

--- FlightReservationSystem/FlightReservationLibrary/src/main/java/pojo/Passenger.java
+++ FlightReservationSystem/FlightReservationLibrary/src/main/java/pojo/Passenger.java
@@ -5,11 +5,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Passenger {
+public class Passenger implements Serializable {
     private String firstName;
 
     private String lastName;
