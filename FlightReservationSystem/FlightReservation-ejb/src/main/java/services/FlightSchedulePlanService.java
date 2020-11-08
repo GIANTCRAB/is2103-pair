66b0ccd61e20c88ce3ec6e84fd7aac23e7030121
Blueberry <55194781+blueberry-byte@users.noreply.github.com>
2020-11-09 03:12:44 +0800

    Added flight schedule plan service

---
+++ FlightReservationSystem/FlightReservation-ejb/src/main/java/services/FlightSchedulePlanService.java
@@ -0,0 +1,61 @@
+package services;
+
+import exceptions.InvalidConstraintException;
+import entities.FlightSchedule;
+import entities.FlightSchedulePlan;
+import entities.FlightSchedulePlanType;
+import java.sql.Date;
+import java.sql.Time;
+import java.util.List;
+import java.util.Set;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import lombok.NonNull;
+
+
+@LocalBean
+@Stateless
+public class FlightSchedulePlanService {
+    @PersistenceContext(unitName = "frs")
+    private EntityManager em;
+    
+    private final ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+    private final Validator validator = validatorFactory.getValidator();
+    
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public FlightSchedulePlan create(@NonNull FlightSchedulePlanType flightSchedulePlanType, @NonNull Date recurrentEndDate) throws InvalidConstraintException {
+        final FlightSchedulePlan flightSchedulePlan = new FlightSchedulePlan();
+        flightSchedulePlan.setFlightSchedulePlanType(flightSchedulePlanType);
+        flightSchedulePlan.setRecurrentEndDate(recurrentEndDate);
+
+        Set<ConstraintViolation<FlightSchedule>> violations = this.validator.validate(flightSchedulePlan);
+        if (!violations.isEmpty()) {
+            throw new InvalidConstraintException(violations.toString());
+        }
+
+        em.persist(flightSchedulePlan);
+        em.flush();
+
+        return flightSchedulePlan;
+    }
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public void associateFlightSchedules(FlightSchedulePlan flightSchedulePlan, List<FlightSchedule> flightSchedules) {
+        flightSchedulePlan.setFlightSchedules(flightSchedules);
+        flightSchedules.forEach(f -> f.setFlightSchedulePlan(flightSchedulePlan));
+    }
+    
+    public List<FlightSchedulePlan> getFlightSchedulePlans() {
+        TypedQuery<FlightSchedulePlan> searchQuery = em.createQuery("SELECT fsp FROM FlightSchedule fsp JOIN fsp.flightSchedules fs ORDER BY fs.flight.flightCode ASC, fs.flight.date DESC", FlightSchedulePlan.class);
+        
+        return searchQuery.getResultList();
+    }
+}
