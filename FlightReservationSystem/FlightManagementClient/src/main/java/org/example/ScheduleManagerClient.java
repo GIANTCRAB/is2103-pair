3a4776c448fc9afc74f5350dd49dee2bb0250f06
Blueberry <55194781+blueberry-byte@users.noreply.github.com>
2020-11-10 05:14:27 +0800

    Add schedule manager client

---
+++ FlightReservationSystem/FlightManagementClient/src/main/java/org/example/ScheduleManagerClient.java
@@ -0,0 +1,52 @@
+package org.example;
+
+import controllers.FlightBeanRemote;
+import controllers.FlightRouteBeanRemote;
+import controllers.AircraftConfigurationBeanRemote;
+import controllers.FlightSchedulePlanBeanRemote;
+import entities.Employee;
+import java.util.Scanner;
+import lombok.AccessLevel;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class ScheduleManagerClient implements SystemClient {
+    @NonNull
+    private final Scanner scanner;
+    @NonNull
+    private final Employee authenticatedEmployee;
+    @NonNull
+    private final FlightBeanRemote flightBeanRemote;
+    @NonNull
+    private final FlightRouteBeanRemote flightRouteBeanRemote;
+    @NonNull
+    private final AircraftConfigurationBeanRemote aircraftConfigurationBeanRemote;
+    @NonNull
+    private final FlightSchedulePlanBeanRemote flightSchedulePlanBeanRemote;
+    
+    
+    @Override
+    public void runApp() {
+        this.scanner = new Scanner(System.in);
+
+        boolean loop = true;
+
+        while (loop) {
+            System.out.println("*** Schedule Manager Client ***");
+            System.out.println("1: Flight Client");
+            System.out.println("2: Flight Schedule Plan Client");
+            final int option = this.scanner.nextInt();
+
+            if (option == 1) {
+                return new FlightClient(this.scanner, this.authenticatedEmployee, flightBeanRemote, flightRouteBeanRemote);
+            } else if (option == 2) {
+                return new FlightSchedulePlanClient(this.scanner, this.authenticatedEmployee, flightBeanRemote, flightRouteBeanRemote);
+            } else {
+                loop = false;
+            }
+        }
+
+        this.scanner.close();
+    }
+}
