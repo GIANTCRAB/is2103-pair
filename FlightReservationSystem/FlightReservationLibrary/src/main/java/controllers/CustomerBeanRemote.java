05ab3f263aca6c59b27b9071765c5b047bdd3e3b
Huiren Woo <giantcrabby@gmail.com>
2020-11-10 04:32:33 +0800

    Add customer bean interface

---
+++ FlightReservationSystem/FlightReservationLibrary/src/main/java/controllers/CustomerBeanRemote.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import entities.*;
+import exceptions.InvalidConstraintException;
+import exceptions.InvalidEntityIdException;
+import exceptions.NotAuthenticatedException;
+import lombok.NonNull;
+import pojo.Passenger;
+
+import javax.ejb.Remote;
+import java.util.List;
+
+@Remote
+public interface CustomerBeanRemote {
+    List<FlightReservation> reserveFlightForPassengers(@NonNull Customer customer,
+                                                       String creditCard,
+                                                       @NonNull FlightSchedule flightSchedule,
+                                                       @NonNull CabinClassType cabinClassType,
+                                                       @NonNull List<Passenger> passengers) throws InvalidEntityIdException, InvalidConstraintException, NotAuthenticatedException;
+
+    List<FlightReservation> getFlightReservations(@NonNull Customer customer) throws InvalidEntityIdException, NotAuthenticatedException;
+}
