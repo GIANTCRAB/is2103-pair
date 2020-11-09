112c6f323c260b14dac3f4921c9a4e1929503e16
Huiren Woo <giantcrabby@gmail.com>
2020-11-10 06:29:49 +0800

    Add constraint checking for FlightReservationPaymentService

--- FlightReservationSystem/FlightReservation-ejb/src/main/java/services/FlightReservationPaymentService.java
+++ FlightReservationSystem/FlightReservation-ejb/src/main/java/services/FlightReservationPaymentService.java
@@ -1,6 +1,7 @@
 package services;
 
 import entities.*;
+import exceptions.InvalidConstraintException;
 import lombok.NonNull;
 
 import javax.ejb.LocalBean;
@@ -9,7 +10,12 @@ import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 import java.util.List;
+import java.util.Set;
 
 @LocalBean
 @Stateless
@@ -17,23 +23,31 @@ public class FlightReservationPaymentService {
     @PersistenceContext(unitName = "frs")
     private EntityManager em;
 
+    private final ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+    private final Validator validator = validatorFactory.getValidator();
+
     @TransactionAttribute(TransactionAttributeType.REQUIRED)
-    public FlightReservationPayment create(@NonNull String creditCardNumber, Customer customer, Partner partner) {
+    public FlightReservationPayment create(@NonNull String creditCardNumber, Customer customer, Partner partner) throws InvalidConstraintException {
         final FlightReservationPayment flightReservationPayment = new FlightReservationPayment();
         flightReservationPayment.setCreditCardNumber(creditCardNumber);
         flightReservationPayment.setCustomer(customer);
         flightReservationPayment.setPartner(partner);
+        Set<ConstraintViolation<FlightReservationPayment>> violations = this.validator.validate(flightReservationPayment);
+        // There are invalid data
+        if (!violations.isEmpty()) {
+            throw new InvalidConstraintException(violations.toString());
+        }
         this.em.persist(flightReservationPayment);
         this.em.flush();
 
         return flightReservationPayment;
     }
 
-    public FlightReservationPayment create(@NonNull String creditCardNumber, Customer customer) {
+    public FlightReservationPayment create(@NonNull String creditCardNumber, Customer customer) throws InvalidConstraintException {
         return this.create(creditCardNumber, customer, null);
     }
 
-    public FlightReservationPayment create(@NonNull String creditCardNumber, Partner partner) {
+    public FlightReservationPayment create(@NonNull String creditCardNumber, Partner partner) throws InvalidConstraintException {
         return this.create(creditCardNumber, null, partner);
     }
 
