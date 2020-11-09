8e4337901ae7e997a236574f21d48f19d519e1de
Huiren Woo <giantcrabby@gmail.com>
2020-11-10 06:24:06 +0800

    Add find Fare by schedule and cabin class

--- FlightReservationSystem/FlightReservation-ejb/src/main/java/services/FareService.java
+++ FlightReservationSystem/FlightReservation-ejb/src/main/java/services/FareService.java
@@ -1,13 +1,35 @@
 package services;
 
+import entities.*;
+import exceptions.InvalidEntityIdException;
+import lombok.NonNull;
+
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 @LocalBean
 @Stateless
 public class FareService {
     @PersistenceContext(unitName = "frs")
     private EntityManager em;
+    @Inject
+    CabinClassService cabinClassService;
+
+    public Fare findByScheduleAndCabinClass(@NonNull FlightSchedule flightSchedule, @NonNull CabinClassType cabinClassType) throws InvalidEntityIdException {
+        final AircraftConfiguration aircraftConfiguration = flightSchedule.getFlight().getAircraftConfiguration();
+        final CabinClassId cabinClassId = new CabinClassId();
+        cabinClassId.setAircraftConfigurationId(aircraftConfiguration.getAircraftConfigurationId());
+        cabinClassId.setCabinClassType(cabinClassType);
+        final CabinClass cabinClass = cabinClassService.findById(cabinClassId);
+
+        TypedQuery<Fare> query = this.em.createQuery("SELECT f FROM Fare f WHERE f.cabinClass.cabinClassId = ?1 AND f.flightSchedule.flightScheduleId = ?2", Fare.class)
+                .setParameter(1, cabinClass.getCabinClassId())
+                .setParameter(2, flightSchedule.getFlightScheduleId());
+
+        return query.getSingleResult();
+    }
 }
