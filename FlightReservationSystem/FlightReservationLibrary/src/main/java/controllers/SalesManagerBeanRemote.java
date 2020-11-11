e320e74ada3deb1476709112b259f164e1d0be9e
Huiren Woo <giantcrabby@gmail.com>
2020-11-12 05:10:57 +0800

    Add draft for SalesManagerBeanRemote

---
+++ FlightReservationSystem/FlightReservationLibrary/src/main/java/controllers/SalesManagerBeanRemote.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import entities.Employee;
+import entities.Flight;
+import entities.FlightReservation;
+import entities.FlightSchedule;
+import exceptions.IncorrectCredentialsException;
+import exceptions.InvalidEntityIdException;
+import exceptions.NotAuthenticatedException;
+import lombok.NonNull;
+
+import javax.ejb.Remote;
+import java.util.List;
+
+@Remote
+public interface SalesManagerBeanRemote {
+    Employee login(String username, String password) throws IncorrectCredentialsException, InvalidEntityIdException;
+
+    List<FlightSchedule> getFlightSchedules(@NonNull Flight flight) throws NotAuthenticatedException, InvalidEntityIdException;
+
+    FlightSchedule getFlightScheduleDetails(@NonNull FlightSchedule flightSchedule) throws NotAuthenticatedException, InvalidEntityIdException;
+
+    List<FlightReservation> getFlightReservations(@NonNull FlightSchedule flightSchedule) throws NotAuthenticatedException, InvalidEntityIdException;
+}
