3c8758b7c93600c5e891127b6c081c6557e5ab02
Huiren Woo <giantcrabby@gmail.com>
2020-11-13 10:02:15 +0800

    Add fare service test data

--- FlightReservationSystem/FlightReservation-ejb/src/main/java/initialisation/TestDataMigrationBean.java
+++ FlightReservationSystem/FlightReservation-ejb/src/main/java/initialisation/TestDataMigrationBean.java
@@ -5,10 +5,15 @@ import lombok.SneakyThrows;
 import services.*;
 
 import javax.annotation.PostConstruct;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.security.enterprise.identitystore.Pbkdf2PasswordHash;
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.sql.Time;
 
 public class TestDataMigrationBean {
     @PersistenceContext(unitName = "frs")
@@ -31,6 +36,7 @@ public class TestDataMigrationBean {
     private Pbkdf2PasswordHash passwordHash;
 
     @SneakyThrows
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
     @PostConstruct
     public void init() {
         // Create Employee
@@ -175,28 +181,63 @@ public class TestDataMigrationBean {
         // NRT, SYD
         final FlightRoute nrtSydFR = this.flightRouteService.create(airport4, airport5);
 
-        this.flightService.create("ML111", sinHkgFR, aircraftConfiguration2);
-        this.flightService.create("ML112", hkgSinFR, aircraftConfiguration2);
+        final Flight ml111 = this.flightService.create("ML111", sinHkgFR, aircraftConfiguration2);
+        final Flight ml112 = this.flightService.create("ML112", hkgSinFR, aircraftConfiguration2);
 
-        this.flightService.create("ML211", sinTpeFR, aircraftConfiguration2);
-        this.flightService.create("ML212", tpeSinFR, aircraftConfiguration2);
+        final Flight ml211 = this.flightService.create("ML211", sinTpeFR, aircraftConfiguration2);
+        final Flight ml212 = this.flightService.create("ML212", tpeSinFR, aircraftConfiguration2);
 
-        this.flightService.create("ML311", sinNrtFR, aircraftConfiguration2);
-        this.flightService.create("ML312", nrtSinFR, aircraftConfiguration2);
+        final Flight ml311 = this.flightService.create("ML311", sinNrtFR, aircraftConfiguration2);
+        final Flight ml312 = this.flightService.create("ML312", nrtSinFR, aircraftConfiguration2);
 
-        this.flightService.create("ML411", hkgNrtFR, aircraftConfiguration2);
-        this.flightService.create("ML412", nrtHkgFR, aircraftConfiguration2);
+        final Flight ml411 = this.flightService.create("ML411", hkgNrtFR, aircraftConfiguration2);
+        final Flight ml412 = this.flightService.create("ML412", nrtHkgFR, aircraftConfiguration2);
 
-        this.flightService.create("ML511", tpeNrtFR, aircraftConfiguration2);
-        this.flightService.create("ML512", nrtTpeFR, aircraftConfiguration2);
+        final Flight ml511 = this.flightService.create("ML511", tpeNrtFR, aircraftConfiguration2);
+        final Flight ml512 = this.flightService.create("ML512", nrtTpeFR, aircraftConfiguration2);
 
-        this.flightService.create("ML611", sinSydFR, aircraftConfiguration2);
-        this.flightService.create("ML612", sydSinFR, aircraftConfiguration2);
+        final Flight ml611 = this.flightService.create("ML611", sinSydFR, aircraftConfiguration2);
+        final Flight ml612 = this.flightService.create("ML612", sydSinFR, aircraftConfiguration2);
 
-        this.flightService.create("ML621", sinSydFR, aircraftConfiguration3);
-        this.flightService.create("ML622", sydSinFR, aircraftConfiguration3);
+        final Flight ml621 = this.flightService.create("ML621", sinSydFR, aircraftConfiguration3);
+        final Flight ml622 = this.flightService.create("ML622", sydSinFR, aircraftConfiguration3);
 
-        this.flightService.create("ML711", sydNrtFR, aircraftConfiguration4);
-        this.flightService.create("ML712", nrtSydFR, aircraftConfiguration4);
+        final Flight ml711 = this.flightService.create("ML711", sydNrtFR, aircraftConfiguration4);
+        final Flight ml712 = this.flightService.create("ML712", nrtSydFR, aircraftConfiguration4);
+
+        // Flight Schedule Plan
+        final CabinClassId cabinClassId1 = new CabinClassId();
+        cabinClassId1.setCabinClassType(CabinClassType.F);
+        cabinClassId1.setAircraftConfigurationId(aircraftConfiguration4.getAircraftConfigurationId());
+        final CabinClass cabinClass1 = this.cabinClassService.findById(cabinClassId1);
+        cabinClassId1.setCabinClassType(CabinClassType.J);
+        final CabinClass cabinClass2 = this.cabinClassService.findById(cabinClassId1);
+        cabinClassId1.setCabinClassType(CabinClassType.Y);
+        final CabinClass cabinClass3 = this.cabinClassService.findById(cabinClassId1);
+
+        final FlightSchedulePlan flightSchedulePlan1 = this.flightSchedulePlanService.createRecurrentFlightSchedule(FlightSchedulePlanType.RECURRENT_WEEKLY,
+                ml711,
+                Date.valueOf("2020-12-1"),
+                Time.valueOf("9:00:00"),
+                Integer.valueOf(14 * 60).longValue(),
+                Date.valueOf("2020-12-31"));
+        this.fareService.create("F001", BigDecimal.valueOf(6500), cabinClass1, flightSchedulePlan1);
+        this.fareService.create("F002", BigDecimal.valueOf(6000), cabinClass1, flightSchedulePlan1);
+        this.fareService.create("J001", BigDecimal.valueOf(3500), cabinClass2, flightSchedulePlan1);
+        this.fareService.create("J002", BigDecimal.valueOf(3000), cabinClass2, flightSchedulePlan1);
+        this.fareService.create("Y001", BigDecimal.valueOf(1500), cabinClass3, flightSchedulePlan1);
+        this.fareService.create("Y002", BigDecimal.valueOf(1000), cabinClass3, flightSchedulePlan1);
+        final FlightSchedulePlan flightSchedulePlan2 = this.flightSchedulePlanService.createRecurrentFlightSchedule(FlightSchedulePlanType.RECURRENT_WEEKLY,
+                ml712,
+                Date.valueOf("2020-12-2"),
+                Time.valueOf("1:00:00"),
+                Integer.valueOf(14 * 60).longValue(),
+                Date.valueOf("2020-12-31"));
+        this.fareService.create("F001", BigDecimal.valueOf(6500), cabinClass1, flightSchedulePlan2);
+        this.fareService.create("F002", BigDecimal.valueOf(6000), cabinClass1, flightSchedulePlan2);
+        this.fareService.create("J001", BigDecimal.valueOf(3500), cabinClass2, flightSchedulePlan2);
+        this.fareService.create("J002", BigDecimal.valueOf(3000), cabinClass2, flightSchedulePlan2);
+        this.fareService.create("Y001", BigDecimal.valueOf(1500), cabinClass3, flightSchedulePlan2);
+        this.fareService.create("Y002", BigDecimal.valueOf(1000), cabinClass3, flightSchedulePlan2);
     }
 }
